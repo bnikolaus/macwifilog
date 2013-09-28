945a2b85dd1456614b21294fc4412ac2e349e7ed
Brent Nikolaus <brent.nikolaus@puppetlabs.com>
2013-09-27 23:02:47 -0700

    added some logic

--- wifilog.py
+++ wifilog.py
@@ -8,17 +8,34 @@ import getpass
 
 interval = 1
 arr =[]
-sudoPassword = getpass.getpass()
-sezitup = "sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/bin/airport"
-command = "airport -I"  # the shell command
-p = os.system('echo %s|sudo -S %s' % (sudoPassword, sezitup))
+
+# setup symlink
+
+try: 
+	with open('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'): pass
+
+except IOError:
+   print "airport is not where I am expecting it cannot setup symlink"
+
+try:
+   with open('/usr/bin/airport'): pass
+  
+except IOError:
+   sudoPassword = getpass.getpass()
+   sezitup = "sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/bin/airport"
+   p = os.system('echo %s|sudo -S %s' % (sudoPassword, sezitup))
+     
+scan = "airport -s"
+command = "airport -I"
 
 #for line in iter(process.stdout.readline,''): 
 #   arr.append(line.rstrip('\n'))
 #   text = map(lambda x: x.strip(" "), arr)
 #   print ','.join(new_arr)
 
-f = open('wifilog.log', 'w')
+today = datetime.datetime.today().strftime('%m-%d-%Y')
+fname = today + "-macwifilog.log"
+f = open(fname, 'w')
 
 def _log(text):
    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True) 
@@ -30,6 +47,9 @@ def _log(text):
    f.write("{date} - {text}\n".format(date=datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"), text=text))
 
 if __name__ == '__main__':
+   startscan = subprocess.Popen(scan, stdout=subprocess.PIPE, stderr=None, shell=True)
+   scanout = startscan.communicate()[0] 
+   print scanout 
    while True:   
       _log('')
       arr =[] 
