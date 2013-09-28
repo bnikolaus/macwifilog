91bb7c310b8df1a9d22eaedded768910646c996b
Brent Nikolaus <brent.nikolaus@puppetlabs.com>
2013-09-27 21:26:48 -0700

    added file output

--- wifilog.py
+++ wifilog.py
@@ -6,6 +6,7 @@ import subprocess
 import time
 import getpass
 
+interval = 1
 arr =[]
 sudoPassword = getpass.getpass()
 sezitup = "sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/bin/airport"
@@ -17,16 +18,18 @@ p = os.system('echo %s|sudo -S %s' % (sudoPassword, sezitup))
 #   text = map(lambda x: x.strip(" "), arr)
 #   print ','.join(new_arr)
 
+f = open('wifilog.log', 'w')
+
 def _log(text):
    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True) 
    for line in iter(process.stdout.readline,''):
       arr.append(line.rstrip('\n'))
       text = map(lambda x: x.strip(" "), arr)
    print "{date} - {text}".format(date=datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"), text=text)
-
+   f.write("{date} - {text}\n".format(date=datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"), text=text))
 
 if __name__ == '__main__':
    while True:   
       _log('')
       arr =[] 
-      time.sleep(5)
+      time.sleep(interval)
