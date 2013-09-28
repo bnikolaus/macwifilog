165166a7e5da8acbbf20cfd9a15e89e78c0581be
Brent Nikolaus <brent.nikolaus@puppetlabs.com>
2013-09-28 00:09:02 -0700

    args

--- wifilog.py
+++ wifilog.py
@@ -5,10 +5,11 @@ import sys
 import subprocess
 import time
 import getpass
+import argparse
 
 interval = 1
 arr =[]
-
+args = ""
 # setup symlink
 try: 
 	with open('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'): pass
@@ -26,6 +27,7 @@ except IOError:
      
 scan = "airport -s"
 command = "airport -I"
+outpath = ""
 
 # Setup for parsing 
 # for line in iter(process.stdout.readline,''): 
@@ -35,7 +37,7 @@ command = "airport -I"
 
 today = datetime.datetime.today().strftime('%m-%d-%Y')
 fname = today + "-macwifilog.log"
-f = open(fname, 'w')
+f = open(outpath + fname, 'w')
 
 def _log(text):
    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True) 
@@ -46,7 +48,13 @@ def _log(text):
    print "{date},{text}".format(date=datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"), text=text)
    f.write("{date},{text}\n".format(date=datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"), text=text))
 
+def parseArgs():
+   parser = argparse.ArgumentParser(description='Display airport interface statistics')
+   parser.add_argument('-o', help="path for output", required=False, dest='outpath')
+   return args
+
 if __name__ == '__main__':
+   args = parseArgs() 
    startscan = subprocess.Popen(scan, stdout=subprocess.PIPE, stderr=None, shell=True)
    scanout = startscan.communicate()[0] 
    print scanout 
